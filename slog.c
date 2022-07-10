f743ec2f955c4eddf748563d1d382c46cfe28912
Emil Engler <me@emilengler.com>
2022-07-10 09:58:27 +0000

    Implement template_init() and template_free()

--- slog.c
+++ slog.c
@@ -18,6 +18,7 @@
 #include <sys/queue.h>
 
 #include <err.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -43,8 +44,83 @@ struct template {
 	char	*footer;
 };
 
-static void	markdown_init(struct markdown *md, FILE *fp);
-static void	markdown_free(struct markdown *md);
+/* Fatal wrappers for libc functions. */
+static FILE	*efopen(const char *, const char *);
+static void	*emalloc(size_t size);
+
+/* Generic helper functions. */
+static char	*aprintf(const char *, ...);
+static char	*read_file(const char *);
+
+/* slog(1) specific functions. */
+static void	 markdown_init(struct markdown *, FILE *);
+static void	 markdown_free(struct markdown *);
+static void	 template_init(struct template *, const char *);
+static void	 template_free(struct template *);
+
+static FILE *
+efopen(const char *path, const char *mode)
+{
+	FILE	*fp;
+
+	if ((fp = fopen(path, mode)) == NULL)
+		err(1, "fopen %s", path);
+
+	return fp;
+}
+
+static void *
+emalloc(size_t size)
+{
+	void	*data;
+
+	if ((data = malloc(size)) == NULL)
+		err(1, "malloc %zu", size);
+
+	return data;
+}
+
+static char *
+aprintf(const char *fmt, ...)
+{
+	char	*str;
+	va_list	 a1, a2;
+	int	 sz;
+
+	va_start(a1, fmt);
+	va_copy(a2, a1);
+
+	if ((sz = vsnprintf(NULL, 0, fmt, a1)) == -1)
+		err(1, "vsnprintf");
+	va_end(a1);
+
+	str = emalloc(sz + 1);
+	if (vsnprintf(str, sz + 1, fmt, a2) == -1)
+		err(1, "vsnprintf");
+	va_end(a2);
+
+	return str;
+}
+
+static char *
+read_file(const char *filename)
+{
+	FILE	*fp;
+	char	*str;
+	long	 sz;
+
+	fp = efopen(filename, "r");
+
+	fseek(fp, 0L, SEEK_END);
+	sz = ftell(fp);
+	fseek(fp, 0L, SEEK_SET);
+
+	str = emalloc(sz + 1);
+	fread(str, 1, sz, fp);
+	str[sz] = '\0';
+
+	return str;
+}
 
 static void
 markdown_init(struct markdown *md, FILE *fp)
@@ -73,6 +149,32 @@ markdown_free(struct markdown *md)
 {
 	lowdown_metaq_free(&md->metaq);
 	free(md->buf);
+}
+
+static void
+template_init(struct template *tmplt, const char *path)
+{
+	char	*path_header, *path_item, *path_footer;
+
+	path_header = aprintf("%s/header", path);
+	path_item = aprintf("%s/item", path);
+	path_footer = aprintf("%s/footer", path);
+
+	tmplt->header = read_file(path_header);
+	tmplt->item = read_file(path_item);
+	tmplt->footer = read_file(path_footer);
+
+	free(path_header);
+	free(path_item);
+	free(path_footer);
+}
+
+static void
+template_free(struct template *tmplt)
+{
+	free(tmplt->header);
+	free(tmplt->item);
+	free(tmplt->footer);
 }
 
 int
